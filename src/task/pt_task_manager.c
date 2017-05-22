dd1b81f2c8037081e3074be2b56389ac776fe415
wujianguo <wujianguo@haixue.com>
2017-05-22 09:46:13 +0800

    demo url

--- src/task/pt_task_manager.c
+++ src/task/pt_task_manager.c
@@ -31,7 +31,7 @@ static void on_timer_expire(uv_timer_t *handle) {
 // gcc src/task/pt_task_manager.c -Isrc -I/usr/local/include
 int main(int argc, char *argv[]) {
     uv_loop_t *loop = uv_default_loop();
-    pt_task *task = pt_create_task(loop, "", "")
+    pt_task *task = pt_create_task(loop, "http://0.s3.envato.com/h264-video-previews/80fad324-9db4-11e3-bf3d-0050569255a8/490527.mp4", "tmp/test.mp4")
 
     uv_timer_t timer_handle;
     uv_timer_init(loop, &timer_handle);
